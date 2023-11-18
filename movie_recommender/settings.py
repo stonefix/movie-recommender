e0d7475b383b71599c0cb7ac28cbdf65a0162c9c
yura yura <stonefiz@gmail.com>
2023-11-18 13:17:04 +0300

    Add .gitignore

--- movie_recommender/settings.py
+++ movie_recommender/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'secret_key'
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['127.0.0.1', 'movie-recommenderrrr.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 DEBUG = True
 
 # Application definition
